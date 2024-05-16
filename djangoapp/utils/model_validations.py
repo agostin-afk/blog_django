f9dccd08c9d527ec3e10ef817135e7d57f32b4d1
Agostinho Ferreira <agostinho008.af45@gmail.com>
2024-05-16 12:35:32 -0300

    field validators, subsc save do Model

---
+++ djangoapp/utils/model_validations.py
@@ -0,0 +1,6 @@
+from django.core.exceptions import ValidationError
+def validate_png(image):
+    if not image.name.lower().endswith('.png'):
+        print("\n\n\n\nimagem enviada com formato errado\n\n\n\n")
+        raise ValidationError('Imagem precisa ser PNG')
+        

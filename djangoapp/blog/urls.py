0f10ad5722c37dd651c00707b367a02a80f59daa
Agostinho Ferreira <agostinho008.af45@gmail.com>
2024-05-19 10:13:56 -0300

    css e html
    não autoral

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -1,10 +1,12 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
-from blog.views import index
+from blog.views import index, page, post
 
 app_name = 'blog'
 
 urlpatterns = [
     path('', index, name='index'),
+    path('page/', page, name='page'),
+    path('post/', post, name='post'),
 ]

ee2d22cf311b2629bd2cab1e06a3b8a335627862
Agostinho Ferreira <agostinho008.af45@gmail.com>
2024-05-21 18:06:55 -0300

    parte interna do Post

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
-from blog.views import index, page, post
+from blog.views import index, page, post, created_by, category
 
 app_name = 'blog'
 
@@ -9,4 +9,7 @@ urlpatterns = [
     path('', index, name='index'),
     path('page/', page, name='page'),
     path('post/<slug:slug>/', post, name='post'),
+    path('created_by/<int:author_pk>/', created_by, name='created_by'),
+    path('category/<slug:slug>/', category, name='category'),
+    
 ]

07b537cea69487431c4db285cd73161f924c69d5
Agostinho Ferreira <agostinho008.af45@gmail.com>
2024-05-27 20:49:23 -0300

    self.kwargs para argumentos de URL

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
-from blog.views import PostListView, page, post, created_by, category, tags, search
+from blog.views import PostListView, page, post, CreatedByListView, category, tags, search
 
 app_name = 'blog'
 
@@ -9,7 +9,7 @@ urlpatterns = [
     path('', PostListView.as_view(), name='index'),
     path('page/', page, name='page'),
     path('post/<slug:slug>/', post, name='post'),
-    path('created_by/<int:author_pk>/', created_by, name='created_by'),
+    path('created_by/<int:author_pk>/', CreatedByListView.as_view(), name='created_by'),
     path('category/<slug:slug>/', category, name='category'),
     path('tags/<slug:slug>/', tags, name='tag'),
     path('search/', search, name='search'),
--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -11,10 +11,8 @@ from django.views.generic import ListView
 PER_PAGE = 9
 
 class PostListView(ListView):
-    model = Post
     template_name = 'blog/pages/index.html'
     context_object_name = 'posts'
-    ordering = '-pk',
     paginate_by = PER_PAGE
     queryset = Post.objects.get_is_published() # type: ignore
     
@@ -23,21 +21,43 @@ class PostListView(ListView):
     #     get_queryset = queryset.filter(is_published=True)
     #     return get_queryset
     
-    
-# def index(request):
-#     posts = Post.objects.get_is_published() # type: ignore
-#     paginator = Paginator(posts, PER_PAGE)
-#     page_number = request.GET.get("page")
-#     page_obj = paginator.get_page(page_number)
+ 
+class CreatedByListView (PostListView):
+    def __init__(self, **kwargs: Any) -> None:
+        super().__init__(**kwargs)
+        self._temp_context: dict[str, Any] = {}
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        user = self._temp_context['user']
+        user_full_name = user.username
+        
+        if user.first_name:
+            user_full_name = f'{user.first_name} {user.last_name} -' 
+        page_title= user_full_name + ' posts -'
+        
+        ctx.update({
+            'page_title': page_title,
+        })
+        return ctx
+    def get_queryset(self) -> QuerySet[Any]:
+        qs = super().get_queryset()
+        qs = qs.filter(created_by__pk=self._temp_context['user'].pk)
+        return qs
 
-#     return render(
-#         request,
-#         'blog/pages/index.html',
-#         {
-#             'page_obj': page_obj,
-#             'page_title': 'Home - ',
-#         }
-#     )
+    def get(self, request, *args, **kwargs):
+        author_pk = self.kwargs.get('author_pk')
+        user = User.objects.filter(pk=author_pk).first()
+
+        if user is None:
+            raise Http404()
+
+        self._temp_context.update({
+            'author_pk': author_pk,
+            'user': user,
+        })
+
+        return super().get(request, *args, **kwargs)
+
 
 def created_by(request, author_pk):
     user = User.objects.filter(pk=author_pk).first()
